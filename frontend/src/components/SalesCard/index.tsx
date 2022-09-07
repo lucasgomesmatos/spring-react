21cd874075f98c9fa3c80323964527a5278a2728
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-09-07 08:45:59 -0300

    order components

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,58 +19,52 @@ export const SalesCard = () => {
           <table className={styles.salesTable}>
             <thead>
               <tr>
-                <th className="show992">ID</th>
-                <th className="show576">Data</th>
+                <th className={styles.show992}>ID</th>
+                <th className={styles.show576}>Data</th>
                 <th>Vendedor</th>
-                <th className="show992">Visitas</th>
-                <th className="show992">Vendas</th>
+                <th className={styles.show992}>Visitas</th>
+                <th className={styles.show992}>Vendas</th>
                 <th>Total</th>
                 <th>Notificar</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
+                <td className={styles.show992}>#341</td>
+                <td className={styles.show576}>08/07/2022</td>
                 <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
+                <td className={styles.show992}>15</td>
+                <td className={styles.show992}>11</td>
                 <td>R$ 55300.00</td>
                 <td>
-                  <div className="dsmeta-red-btn-container">
-                    <div className="dsmeta-red-btn">
-                      <NotificationButton />
-                    </div>
+                  <div className={styles.redBtnContainer}>
+                    <NotificationButton />
                   </div>
                 </td>
               </tr>
               <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
+                <td className={styles.show992}>#341</td>
+                <td className={styles.show576}>08/07/2022</td>
                 <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
+                <td className={styles.show992}>15</td>
+                <td className={styles.show992}>11</td>
                 <td>R$ 55300.00</td>
                 <td>
-                  <div className="dsmeta-red-btn-container">
-                    <div className="dsmeta-red-btn">
-                      <NotificationButton />
-                    </div>
+                  <div className={styles.redBtnContainer}>
+                    <NotificationButton />
                   </div>
                 </td>
               </tr>
               <tr>
-                <td className="show992">#341</td>
-                <td className="show576">08/07/2022</td>
+                <td className={styles.show992}>#341</td>
+                <td className={styles.show576}>08/07/2022</td>
                 <td>Anakin</td>
-                <td className="show992">15</td>
-                <td className="show992">11</td>
+                <td className={styles.show992}>15</td>
+                <td className={styles.show992}>11</td>
                 <td>R$ 55300.00</td>
                 <td>
-                  <div className="dsmeta-red-btn-container">
-                    <div className="dsmeta-red-btn">
-                      <NotificationButton />
-                    </div>
+                  <div className={styles.redBtnContainer}>
+                    <NotificationButton />
                   </div>
                 </td>
               </tr>
